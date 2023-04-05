9566189a904a15e83c0ee90007592ee18ebe1469
Oleksandr Tomashchuk <oleksandr.tomashchuk@salesflow.io>
2023-04-05 18:28:31 +0100

    configure remote terraform state

--- main.tf
+++ main.tf
@@ -1,31 +1,43 @@
+terraform {
+  required_version = ">=0.13"
+  backend "s3" {
+    bucket = "salesflowio-terraform-state"
+    key = "myapp/state.tfstate"
+    region = "us-east-2"
+  }
+}
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags       = {
-    Name : "${var.env_prefix}-vpc"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs                = [var.availability_zone]
+  public_subnets     = [var.subnet_cidr_block]
+  public_subnet_tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
 
-module "myapp-subnet" {
-  source = "./modules/subnet"
-  vpc_id                 = aws_vpc.myapp-vpc.id
-  availability_zone      = var.availability_zone
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-  env_prefix             = var.env_prefix
-  subnet_cidr_block      = var.subnet_cidr_block
-}
-
 module "myapp-webserver" {
-  source = "./modules/webserver"
-  ami_name = var.ami_name
+  source            = "./modules/webserver"
+  ami_name          = var.ami_name
   availability_zone = var.availability_zone
   ec2_instance_type = var.ec2_instance_type
-  env_prefix = var.env_prefix
-  my_ip = var.my_ip
-  ssh_key_path = var.ssh_key_path
-  subnet_id = module.myapp-subnet.subnet.id
-  vpc_id = aws_vpc.myapp-vpc.id
+  env_prefix        = var.env_prefix
+  my_ip             = var.my_ip
+  ssh_key_path      = var.ssh_key_path
+  subnet_id         = module.vpc.public_subnets[0]
+  vpc_id            = module.vpc.vpc_id
 }
