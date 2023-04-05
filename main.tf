7e2a274a69cf9309c3bafc9a8a6231d15505f146
Oleksandr Tomashchuk <oleksandr.tomashchuk@salesflow.io>
2023-04-05 12:47:03 +0100

    create vpc, subnet, igw and ec2 instance

--- main.tf
+++ main.tf
@@ -2,42 +2,148 @@ provider "aws" {
   region = "us-east-2"
 }
 
-variable "cidr_blocks" {
-  description = "cidr blocks and name tags for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name = string
-  }))
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "availability_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "ec2_instance_type" {}
+variable "ssh_key_path" {}
 
-variable ENV_AVAIL_ZONE {}
-
-resource "aws_vpc" "tr-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
-  tags = { 
-    Name: var.cidr_blocks[0].name
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags       = {
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "tr-subnet-1" {
-  vpc_id = aws_vpc.tr-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block
-  availability_zone = var.ENV_AVAIL_ZONE
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags              = {
+    Name : "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-internet-gw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags   = {
+    Name : "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-internet-gw.id
+  }
+
   tags = {
-    Name: var.cidr_blocks[1].name
+    Name : "${var.env_prefix}-main-rtb"
   }
 }
 
-data "aws_vpc" "default-vpc" {
-  default = true
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name : "${var.env_prefix}-sg"
+  }
 }
 
-output "tr-vpc-id" {
-  value = aws_vpc.tr-vpc.id
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
-output "tr-subnet-1-id" {
-  value = aws_subnet.tr-subnet-1.id
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "terraform-key-2"
+  public_key = file(var.ssh_key_path)
 }
-output "tr-subnet-1-arn" {
-  value = aws_subnet.tr-subnet-1.arn
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.ec2_instance_type
+
+  subnet_id              = aws_subnet.myapp-subnet-1.id
+  security_groups        = [aws_security_group.myapp-sg.id]
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone      = var.availability_zone
+
+  associate_public_ip_address = true
+
+  key_name = aws_key_pair.ssh-key.key_name
+  
+  user_data = file("./shell/init-server.sh")
+
+  tags = {
+    Name : "${var.env_prefix}-myapp-server"
+  }
 }
+
+output "aws-ami-id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2-public-ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+#create route table and associate it with subnet
+#resource "aws_route_table" "myapp-route-table" {
+#  vpc_id = aws_vpc.myapp-vpc.id
+#
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.myapp-internet-gw.id
+#  }
+#
+#  tags = {
+#    Name: "${var.env_prefix}-rtb"
+#  }
+#}
+#
+#resource "aws_route_table_association" "myapp-rtb-a" {
+#  route_table_id = aws_route_table.myapp-route-table.id
+#  subnet_id = aws_subnet.myapp-subnet-1.id
+#}
